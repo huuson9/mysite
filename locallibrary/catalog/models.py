1ff19aae334a7c0a2b48e1323ea3fa1b293b3b99
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-09-05 14:56:17 +0700

    part4

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -13,7 +13,6 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
-
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -31,8 +30,14 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
-
-
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+    
+    
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text=_('Unique ID for this particular bookacross '
